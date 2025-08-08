7feb8a193474655466f6323c391df186ebc4a975
JUSTIN RUBIN <Justinrubin328@gmail.com>
2025-08-08 09:32:04 +0800

    Calculator.dart

--- Calculator.dart
+++ Calculator.dart
@@ -1,15 +1,20 @@
-void main() {
-int num1 = 28; 
-int num2 = 3; 
-  
-int sum = num1 + num2;
-int diff = num1 - num2;
-int mul = num1 * num2;
-double div = num1 / num2; 
-  
-print("The sum is $sum");
-print("The diff is $diff");
-print("The mul is $mul");
-print("The div is $div");
+void main() 
+  int num1 = 28;
+  int num2 = 3;
 
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2; 
+  int intDiv = num1 ~/ num2;
+  int remainder = num1 % num2; 
+
+  print("=== Basic Arithmetic Operations ===");
+  print("Numbers: $num1 and $num2");
+  print("Sum: $sum");
+  print("Difference: $diff");
+  print("Product: $mul");
+  print("Division (double): ${div.toStringAsFixed(2)}");
+  print("Division (integer): $intDiv");
+  print("Remainder: $remainder");
 }
